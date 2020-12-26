30a82ef25d1eaa850267a6b4d6c575ba5474d10f
clust3rr <thg.santos@outlook.com>
2020-12-25 23:14:24 -0300

    added the remaining images of the pieces and made an algorithm for selecting the images of the pieces

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,8 +1,18 @@
 import { useState } from 'react';
 import Socket from '../services/socket';
 import { Match } from '../views/app';
-import PawnWhite from '../assets/Pieces/ic_pawn_white.png';
-import PawnBlack from '../assets/Pieces/ic_pawn_black.png';
+import BishopWhite from '../assets/Pieces/bishop_white.svg';
+import BishopBlack from '../assets/Pieces/bishop_black.svg';
+import KingWhite from '../assets/Pieces/king_white.svg';
+import KingBlack from '../assets/Pieces/king_black.svg';
+import KnightWhite from '../assets/Pieces/knight_white.svg';
+import KnightBlack from '../assets/Pieces/knight_black.svg';
+import PawnWhite from '../assets/Pieces/pawn_white.svg';
+import PawnBlack from '../assets/Pieces/pawn_black.svg';
+import QueenWhite from '../assets/Pieces/queen_white.svg';
+import QueenBlack from '../assets/Pieces/queen_black.svg';
+import RookWhite from '../assets/Pieces/rook_white.svg';
+import RookBlack from '../assets/Pieces/rook_black.svg';
 
 interface Props {
   socket: Socket
@@ -49,6 +59,31 @@ export default function ChessBoard({ socket, match }: Props) {
     return `${position.i}${position.j}`;
   }
 
+  function selectePiece(symbol: string) {
+    let options = { white: '', black: '' }
+    switch (symbol) {
+      case 'K':
+        options = { white: KingWhite, black: KingBlack }
+        break;
+      case 'Q':
+        options = { white: QueenWhite, black: QueenBlack }
+        break;
+      case 'R':
+        options = { white: RookWhite, black: RookBlack }
+        break;
+      case 'B':
+        options = { white: BishopWhite, black: BishopBlack }
+        break;
+      case 'N':
+        options = { white: KnightWhite, black: KnightBlack }
+        break;
+      case 'P':
+        options = { white: PawnWhite, black: PawnBlack }
+        break;
+    }
+    return options;
+  }
+
   function onSelectedPiece(i: number, j: number) {
     const chessPosition = positionToChessPosition(i, j);
 
@@ -80,9 +115,10 @@ export default function ChessBoard({ socket, match }: Props) {
                   <div>
                     {(() => {
                       if (cell) {
-                        const piece = (cell.color === Color.White) ? PawnWhite : PawnBlack;
+                        const pieces = selectePiece(cell.symbol)
+                        const piece = (cell.color === Color.White) ? pieces.white : pieces.black;
                         return (
-                          <div>
+                          <div className="pieceContainer">
                             <img className="pieceImg" src={piece} alt="piece" />
                           </div>
                         );
